a930dc4eebf7431049ee2acb7ea940f3b30e0dba
sravan <sravanyadala7@gmail.com>
2026-01-31 17:18:00 +0530

    Fix Express 5 wildcard with named parameter

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // Fallback for SPA routing
-app.get('(.*)', (req, res) => {
+app.get('/:path*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
